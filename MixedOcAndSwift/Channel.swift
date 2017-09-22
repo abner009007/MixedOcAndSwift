281ba3de5cb178a46ff8dfc5190043be898fec1f
abner <letian@letiandeMacBook-Air.local>
2017-09-22 17:27:53 +0800

    swift
    swift

---
+++ MixedOcAndSwift/Channel.swift
@@ -0,0 +1,18 @@
+//
+//  Channel.swift
+//  MixedOcAndSwift
+//
+//  Created by 乐天 on 2017/9/22.
+//  Copyright © 2017年 乐天. All rights reserved.
+//
+
+import UIKit
+
+public class Channel: NSObject {
+
+    
+    public func testChannel(name:String)->(String){
+        let myName : String = name+"abner";
+        return myName;
+    }
+}
--- MixedOcAndSwift/ViewController.m
+++ MixedOcAndSwift/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "MixedOcAndSwift-Swift.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    Channel * channel = [[Channel alloc]init];
+    NSString * name = [channel testChannelWithName:@"abner"];
+    
+    NSLog(@"====abner=====%@",name);
 }
 
 
